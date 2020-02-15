2a47656a4f412c7f55b6b36ea3941612746e4613
Josh Martin <joshmartin5001@gmail.com>
2020-02-15 18:48:54 -0500

    Added commands for running program and made run headless

--- src/puppeteerConfiguation.ts
+++ src/puppeteerConfiguation.ts
@@ -1,16 +1,16 @@
 import { LaunchOptions } from 'puppeteer';
 
 const config: LaunchOptions = {
-  // executablePath: process.env.CHROMEPATH,
-  args: ['--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--disable-accelerated-2d-canvas',
-    '--disable-gpu',
-    '--window-size=1920x1080'] ,
-  // devtools: true,
-  // headless: false,
-  slowMo: 10,
+    executablePath: process.env.CHROMEPATH,
+    args: ['--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--disable-gpu',
+        '--window-size=1920x1080'] ,
+        // devtools: true,
+        headless: true,
+        slowMo: 10,
 };
 
 export default  config;
